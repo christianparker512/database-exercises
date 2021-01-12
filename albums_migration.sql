1dbee6e164f48e2197431c6be64bd5eda11cf8c2
Christian Parker <christianparker512@gmail.com>
2021-01-12 09:28:55 -0600

    mysql basic statements exercises

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,12 @@
 USE codeup_test_db;
 
 CREATE TABLE albums(
-    id INT,
-    artist VARCHAR(255),
-    name VARCHAR(255),
-    release_date INT,
-    sales float,
-    genre VARCHAR(255)
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50) NOT NULL DEFAULT 'unknown',
+    name VARCHAR(50) NOT NULL DEFAULT 'untitled',
+    release_date INT UNSIGNED,
+    sales FLOAT UNSIGNED,
+    genre VARCHAR(100),
+    PRIMARY KEY(id)
 
 );
