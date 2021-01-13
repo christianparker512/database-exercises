5ace5414e12898a3b5924a1a09db6c08f51db4d0
Christian Parker <christianparker512@gmail.com>
2021-01-13 11:27:36 -0600

    Limit exercises 3 and 4 - using LIMIT and OFFSET

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,6 +1,24 @@
 USE employees;
 
+
+# List the first 10 distinct last name sorted in descending order. Your result should look like this:
 SELECT DISTINCT last_name
 FROM employees
 ORDER BY last_name DESC
 limit 10;
+
+# Find your query for employees born on Christmas and hired in the 90s from order_by_exercises.sql. Update it to find just the first 5 employees. Their names should be:
+SELECT CONCAT (first_name, ' ', last_name) AS 'The First 5 employees born on Christmas and hired in the 90''s'
+FROM employees
+WHERE birth_date LIKE '%12-25'
+and hire_date LIKE  '199%'
+ORDER BY  birth_date, hire_date DESC
+LIMIT 5;
+
+# Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the query to find the tenth page of results. The employee names should be:
+SELECT CONCAT (first_name, ' ', last_name) AS 'The First 5 employees born on Christmas and hired in the 90''s'
+FROM employees
+WHERE birth_date LIKE '%12-25'
+  and hire_date LIKE  '199%'
+ORDER BY  birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
