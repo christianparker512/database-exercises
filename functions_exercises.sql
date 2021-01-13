afa4431b62398aae86205b33758fc631a1694f7b
Christian Parker <christianparker512@gmail.com>
2021-01-13 12:11:53 -0600

    Functions exercise #3 - concatting first and last name for employes hired in the 90s using the datedff method to find out how many days they've been working there and also have the birthdate of christmas

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -28,3 +28,13 @@ FROM employees
 WHERE last_name LIKE 'e%' AND last_name LIKE '%e'
 ORDER BY last_name;
 
+SELECT *
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+# For your query of employees born on Christmas and hired in the 90s, use datediff() to find how many days they have been working at the company (Hint: You will also need to use now() or curdate())
+SELECT CONCAT(first_name, ' ', last_name), DATEDIFF(curdate(), hire_date)
+FROM employees
+WHERE birth_date LIKE '%-12-25'
+  AND hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+ORDER BY DATEDIFF(curdate(), hire_date);
