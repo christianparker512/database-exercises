d4acd75f82c84dbff6a4eb3db20ec99aa7427cba
Christian Parker <christianparker512@gmail.com>
2021-01-15 13:29:00 -0600

    subqueries exercise #2 all employees with first name of Aamod broken up into individual titles

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -10,10 +10,20 @@ WHERE emp_no IN (
 )
 LIMIT 10;
 
-
+# Find all the employees with the same hire date as employee 101010 using a subquery.
+# 69 Rows
 SELECT emp_no, CONCAT(e.first_name, ' ', e.last_name) AS full_name, hire_date
 FROM employees AS e
 WHERE hire_date IN (
     SELECT hire_date
     FROM employees
     WHERE emp_no = 101010);
+
+# 2. Find all the titles held by all employees with the first name Aamod.
+# 314 total titles, 6 unique titles
+SELECT title, COUNT(title)
+FROM titles
+WHERE emp_no IN (SELECT emp_no
+                 FROM employees
+                 WHERE first_name = 'Aamod')
+group by title;
