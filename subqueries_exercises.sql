176aaaedd70ec2c760dd79f4fbdfbb197810c9b3
Christian Parker <christianparker512@gmail.com>
2021-01-15 13:13:56 -0600

    subqueries exercise 1. all employees hired on the same date as emp_no 101010, listed

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -10,3 +10,10 @@ WHERE emp_no IN (
 )
 LIMIT 10;
 
+
+SELECT emp_no, CONCAT(e.first_name, ' ', e.last_name) AS full_name, hire_date
+FROM employees AS e
+WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010);
