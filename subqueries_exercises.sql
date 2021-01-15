e5f3c050a7e2002889d87f6724399d6a3d7d38c9
Christian Parker <christianparker512@gmail.com>
2021-01-15 14:08:26 -0600

    Subqueries exercise #3

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -27,3 +27,26 @@ WHERE emp_no IN (SELECT emp_no
                  FROM employees
                  WHERE first_name = 'Aamod')
 group by title;
+
+SELECT first_name, last_name, gender
+    FROM employees
+    where emp_no in (
+        SELECT emp_no
+        FROM dept_manager
+        WHERE to_date = '9999-01-01')
+        AND gender = 'F'
+         ORDER BY first_name;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
