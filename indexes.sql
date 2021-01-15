45ee882f597eda506657361784c28d7aee0da78f
Christian Parker <christianparker512@gmail.com>
2021-01-15 07:50:02 -0600

    indexes and index lecture

--- indexes.sql
+++ indexes.sql
@@ -3,6 +3,7 @@ USE codeup_test_db;
 SHOW TABLES;
 
 SELECT * FROM albums;
+
 DESCRIBE albums;
 
 ALTER TABLE albums
@@ -13,4 +14,6 @@ VALUES ('Test Artist 3', 'Test Album asdf', 2000, 11, 'Test2 Genre');
 
 INSERT INTO albums (artist, name, release_date, sales, genre)
 VALUES ('Test Artist 3', 'Test Album asdf', 2000, 11, 'Test2 Genre');
+
+
 
