345dd4d0aad34382a625243041c0a68978f6b781
Christian Parker <christianparker512@gmail.com>
2021-01-18 18:07:42 -0600

    Associative tables and joins and aliases

--- database_design_lecture.sql
+++ database_design_lecture.sql
@@ -362,6 +362,14 @@ select users.name as user_name, roles.name as role_name
 from users
 join roles on users.role_id = roles.id;
 
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS full_name, d.dept_name
+FROM employees as e
+         JOIN dept_emp as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
+
 1. All tables except associative tables should have a single primary key called id.
 
 	```
